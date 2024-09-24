47db4d0fcc024a8c8824791534d3791b1da590a1
wjx7794 <wjx7794@163.com>
2024-09-24 10:36:01 +0800

    feat: test

--- demo1/pm2.config.js
+++ demo1/pm2.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: './dist/main.js',
       instances: 'max', // 启动最大实例数
       exec_mode: 'cluster', // 使用集群模式
-      watch: true, // 是否监视文件变化
+      watch: false, // 是否监视文件变化
       env: {
         NODE_ENV: 'production',
       },
