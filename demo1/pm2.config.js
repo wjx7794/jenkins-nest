88ca327fee189779b794563d221db15dd6b4ee6e
wjx7794 <wjx7794@163.com>
2024-09-24 09:32:02 +0800

    feat: add pm2 config

---
+++ demo1/pm2.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'my-nest-app',
+      script: './dist/main.js',
+      instances: 'max', // 启动最大实例数
+      exec_mode: 'cluster', // 使用集群模式
+      watch: true, // 是否监视文件变化
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
